d6160c37ecfe69a67f40925b47d05b5f8f201ac9
Youcef Boubidi <youcefboubidi3@gmail.com>
2024-11-01 00:29:19 +0100

    asds

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000; // Use process.env.PORT for Azure
 
+// Serve static files from the ./client/dist directory
+app.use(express.static(path.join(__dirname, 'client', 'dist')));
+
+// Serve index.html for the root URL
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 });
 
+// Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
